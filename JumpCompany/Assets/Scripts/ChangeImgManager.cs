e16e6d5c37629fb9c4d91d0cc2eb1861ebc61094
kogito0325 <qasw405@gmail.com>
2022-08-01 00:20:43 +0900

    08/01 캐릭터 선택창 업데이트 // 스크립트 다수 바뀜 확인부탁

---
+++ JumpCompany/Assets/Scripts/ChangeImgManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeImgManager : MonoBehaviour
+{
+    public Sprite[] records;
+    public Sprite[] characters;
+    public int characterIndex = 0;
+
+    public GameObject nowRecord;
+    public GameObject nowCharacter;
+
+    public void Start()
+    {
+        ChangeCharacter();
+    }
+
+
+    public void ChangeCharacter()
+    {
+        nowRecord.GetComponent<Image>().sprite = records[characterIndex];
+        nowCharacter.GetComponent<Image>().sprite = characters[characterIndex];
+    }
+
+    public void SetNextIndex()
+    {
+        if (characterIndex < characters.Length - 1)
+            characterIndex += 1;
+        ChangeCharacter();
+    }
+
+    public void SetPreviousIndex()
+    {
+        if (characterIndex > 0)
+            characterIndex -= 1;
+        ChangeCharacter();
+    }
+}
